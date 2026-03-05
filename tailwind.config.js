6a18d5e1b84984a6e077a18261994de222b1f5f9
josemanu.formacion.it <josemanu.formacion.it@gmail.com>
2026-03-05 23:19:21 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,html}"
+  ],
   theme: {
     extend: {},
   },
+  darkMode: "class",
   plugins: [],
-}
+};
